1cb4e505fa8abf2dba315206ba6a613fd9deb25c
Avi Aryan <avi.aryan123@gmail.com>
2017-11-06 20:35:16 +0530

    added join and leave channels

--- views.py
+++ views.py
@@ -2,7 +2,8 @@ from app import app
 from flask import request, jsonify
 
 
-channels = []
+channels = {}
+channel_members = {}
 
 
 @app.route('/')
@@ -12,10 +13,22 @@ def home():
 
 @app.route('/channels/<name>', methods=['POST'])
 def create_channel(name):
-    channels.append(name)
+    channels[name] = {'count': 0}
     return jsonify({'status': 'ok'})
 
 
 @app.route('/channels')
 def list_channels():
-    return jsonify({'channels': channels})
+    return jsonify(channels)
+
+
+@app.route('/join_channel/<name>', methods=['POST'])
+def join_channel(name):
+    channels[name]['count'] += 1
+    return jsonify({'status': 'ok'})
+
+
+@app.route('/leave_channel/<name>', methods=['POST'])
+def leave_channel(name):
+    channels[name]['count'] -= 1
+    return jsonify({'status': 'ok'})
